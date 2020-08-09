2c8c9478e9954f0ff9b96402d60b7d06848b4279
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-08-08 20:21:44 -0400

    commit so can move to different computer

---
+++ NiliBlazorAppWithAuthentication/Common/WellKnownValues.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NiliBlazorAppWithAuthentication.Common
+{
+    public static class WellKnownValues
+    {
+        public string AppTitle { get; set; }
+    }
+}
