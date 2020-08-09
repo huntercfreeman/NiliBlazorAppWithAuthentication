e67f516f5e3b5435b7c3fb5088c22a87e410ffbc
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-08-09 05:12:14 -0400

    dragging changes

--- NiliBlazorAppWithAuthentication/Pages/Medications.razor.cs
+++ NiliBlazorAppWithAuthentication/Pages/Medications.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using NiliBlazorAppWithAuthentication.Models;
 using NiliBlazorAppWithAuthentication.Services.Interfaces;
+using NiliBlazorAppWithAuthentication.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,22 @@ namespace NiliBlazorAppWithAuthentication.Pages
     {
         [Inject]
         public IMedicationService MedicationService { get; set; }
+        
+        public Medication MedicationBeingDragged { get; set; }
+        public bool IsHoveringPillContainer { get; set; }
+        public Medication MedicationBeingEdited { get; set; }
         public List<Medication> MedicationsList { get; set; }
         protected override void OnInitialized()
         {
             base.OnInitialized();
             MedicationsList = MedicationService.GetMedications();
+        }
+        private void OnDragEnd()
+        {
+            if(MedicationBeingDragged != null) MedicationBeingEdited = MedicationBeingDragged;
+            MedicationBeingDragged = null;
+            StateHasChanged();
+            Console.WriteLine("Hello World");
         }
     }
 }
